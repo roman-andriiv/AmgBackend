2a1f4db855046609fb6a8da786df12bfb63805f3
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-05 10:48:06 +0200

    Configuring Customer Entity

--- src/main/java/com/andriiv/amgbackend/customer/Customer.java
+++ src/main/java/com/andriiv/amgbackend/customer/Customer.java
@@ -1,18 +1,33 @@
 package com.andriiv.amgbackend.customer;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * Created by Roman Andriiv (05.08.2023 - 09:07)
  */
+@Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private Integer age;
+
+    public Customer(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
 }
