aec3983e4cd299f79afb320722b8c0f8a4dccb05
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-05 09:03:36 +0200

    Initial commit

---
+++ src/main/java/com/andriiv/amgbackend/AmgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andriiv.amgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmgBackendApplication.class, args);
+	}
+
+}
