1fcc9f24dcb862582aa9b3c6daf6838a1ae70866
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-09 19:45:13 +0200

    [testing] Add JdbcDataAccessServiceTest class. Configure JdbcTemplate

--- src/test/java/com/andriiv/amgbackend/AbstractTestcontainers.java
+++ src/test/java/com/andriiv/amgbackend/AbstractTestcontainers.java
@@ -2,11 +2,15 @@ package com.andriiv.amgbackend;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Roman Andriiv (09.08.2023 - 18:42)
@@ -34,5 +38,19 @@ public abstract class AbstractTestcontainers {
         registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    private static DataSource getDataSource() {
+
+        return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
+    }
+
+    protected static JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
     }
 }
