6f5b261193ed50314f071bfc3829cfa0bae14d1c
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-09 17:04:12 +0200

    [testing] Apply Flyway migration and register Datasource properties

--- src/test/java/com/andriiv/amgbackend/TestcontainersTest.java
+++ src/test/java/com/andriiv/amgbackend/TestcontainersTest.java
@@ -1,6 +1,10 @@
 package com.andriiv.amgbackend;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Created by Roman Andriiv (09.08.2023 - 14:56)
  */
 @Testcontainers
+@SpringBootTest
 public class TestcontainersTest {
     @Container
     public static final PostgreSQLContainer<?> postgreSQLContainer =
@@ -19,9 +24,26 @@ public class TestcontainersTest {
                     .withUsername("andriiv")
                     .withPassword("password");
 
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDbMigrationWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()).load();
+        flyway.migrate();
+        System.out.println();
     }
 }
