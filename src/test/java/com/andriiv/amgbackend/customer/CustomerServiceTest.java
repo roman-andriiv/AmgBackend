7f55455bbc5d9c8074e2598b44428c99995899b9
Roman Andriiv <roman.andriyiv@gmail.com>
2023-08-13 11:01:07 +0200

    [testing] Testing Business Layer(CustomerService): test getAllCustomer() and getCustomer()

--- src/test/java/com/andriiv/amgbackend/customer/CustomerServiceTest.java
+++ src/test/java/com/andriiv/amgbackend/customer/CustomerServiceTest.java
@@ -1,10 +1,18 @@
 package com.andriiv.amgbackend.customer;
 
+import com.andriiv.amgbackend.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Roman Andriiv (12.08.2023 - 15:32)
@@ -23,20 +31,37 @@ class CustomerServiceTest {
 
     @Test
     void getAllCustomers() {
-        //Given
-
         //When
-
+        underTest.getAllCustomers();
         //Then
+        verify(customerDao).selectAllCustomers();
     }
 
     @Test
-    void getCustomer() {
+    void canGetCustomer() {
         //Given
+        int id = 1;
+        Customer customer = new Customer(id, "Roman",
+                "roman.andriiv.dev@gmail.com", 27);
+        when(customerDao.selectCustomerById(id)).thenReturn(Optional.of(customer));
 
         //When
+        Customer actual = underTest.getCustomer(id);
+        //Then
+        assertThat(actual).isEqualTo(customer);
+    }
+
+    @Test
+    void getCustomer_willThrowException() {
+        //Given
+        int id = 1000;
+        when(customerDao.selectCustomerById(id)).thenReturn(Optional.empty());
 
         //Then
+        assertThatThrownBy(() -> underTest.getCustomer(id))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("customer with id [%s] not found".formatted(id));
+
     }
 
     @Test
